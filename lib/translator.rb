9194b71215278dda6bbcab371db71e51f3037614
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-09 01:15:16 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,15 @@ require "yaml"
 require "pry"
 
 def load_library
-  # code goes here
+  emojis = YAML.load_file(path)
+  hash = {"get_meaning" => {}, "get_emoticon" => {} }
+    emojis.each do |meaning, emot|
+      jap = emot[1]
+      eng = emot[0]
+      hash['get_meaning'][jap] = meaning
+      hash['get_emoticon'][eng] = jap
+    end
+  hash
 end
 
 def get_japanese_emoticon
